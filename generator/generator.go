0836bd7a5317b24cb020be0d7bc481e7138ba6b9
Dmitry Savvateev <dsv@eml.cc>
2020-10-20 13:18:57 +0300

    use 'flag' to parse params

--- generator/generator.go
+++ generator/generator.go
@@ -1,42 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
-	"os"
-	"strconv"
 )
 
-func intArg(i int) int {
-	input := os.Args[i]
-	num, err := strconv.Atoi(input)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Invalid number: %s\n", input)
-		os.Exit(1)
-	}
-	return num
-}
-
 func main() {
-	if len(os.Args) < 2 {
-		println("Specify the number of strings to generate")
-		os.Exit(1)
-	}
-	num := intArg(1)
 
-	max := 160
-	if len(os.Args) > 2 {
-		max = intArg(2)
-	}
+	num := flag.Int("n", 10, "Number of strings to generate")
+	max := flag.Int("m", 120, "Max string length")
+	seed := flag.Int64("s", 1, "Random generator's seed")
 
-	var seed int64 = 1
-	if len(os.Args) > 3 {
-		seed = int64(intArg(3))
-	}
-	rand.Seed(seed)
+	flag.Parse()
 
-	for i := 0; i < num; i++ {
-		strlen := rand.Intn(max)
+	rand.Seed(*seed)
+
+	for i := 0; i < *num; i++ {
+		strlen := rand.Intn(*max)
 		s := make([]byte, strlen)
 		for i := 0; i < strlen; i++ {
 			s[i] = byte(rand.Intn('z'-'a'+1) + 'a')
